ae19a30ccb20acca1ced4575ce1399753f35dad8
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-14 19:16:15 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -43,12 +43,13 @@ def using_uniq(array)
 end
 
 #returns an array of strings given an array of arrays
-def using_flatten
-  
+def using_flatten(array)
+  array.flatten
 end
 
 def using_delete(array, string)
+  array.delete(string)
 end
 
 def using_delete_at(array, integer)
-
+  
