459afab4325911e4e48bbcecb7355488f7dd86b8
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-14 19:05:55 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,1 +1,10 @@
-def using_push(array, )
+#add to end of array
+def using_push(array, string)
+  array.push(string)
+end
+
+#add to beginning of array
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
